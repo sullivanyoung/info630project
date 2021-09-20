fd3cc3bdf07001896f54f20948717f26747adf54
Sullivan Young <sullivangyoung@gmail.com>
2021-09-20 16:00:28 -0400

    adding tailwinds for styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/layout/navbar';
+import Footer from './components/layout/footer';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Footer />
     </Router>
   );
 }
