9d388ebc4c1b6cdc1d97dcae1f63a1503e931a91
Albin K C <8876252+albinkc@users.noreply.github.com>
2021-12-05 22:50:36 -0500

    pretty (subjective) add to cart buttons

--- src/components/actionButton.jsx
+++ src/components/actionButton.jsx
@@ -1,7 +1,7 @@
 export const ActionButton = ({ currentCount, addProduct }) => {
 	return (
 		<button
-			className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-10 rounded whitespace-nowrap	"
+			className="bg-gradient-to-r from-green-300 to-green-400 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-10 whitespace-nowrap	"
 			onClick={() => addProduct(currentCount + 1)}
 		>
 			Add to Cart
